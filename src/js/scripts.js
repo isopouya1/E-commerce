9832a3eb76aa8873d7f0593e9aaa911957ae56f1
isopouya1 <pouya2402@gmail.com>
2023-08-16 16:40:43 +0330

    Edit login page

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -3,13 +3,13 @@ AOS.init({
 })
 
 document.querySelector("#overlayToggle").addEventListener("click", (e) => {
-    e.preventDefault();
-    document.querySelector("#overlayMenu").classList.toggle("hidden");
-    document.querySelector("#overlayMenu").classList.toggle("flex");
-  });
-  
-  document.querySelector("#closeOverlayToggle").addEventListener("click", (e) => {
-    e.preventDefault();
-    document.querySelector("#overlayMenu").classList.remove("flex");
-    document.querySelector("#overlayMenu").classList.add("hidden");
-  });
+  e.preventDefault();
+  document.querySelector("#overlayMenu").classList.toggle("right-[-100%]");
+  document.querySelector("#overlayMenu").classList.toggle("right-0");
+});
+
+document.querySelector("#closeOverlayToggle").addEventListener("click", (e) => {
+  e.preventDefault();
+  document.querySelector("#overlayMenu").classList.remove("right-0");
+  document.querySelector("#overlayMenu").classList.add("right-[-100%]");
+});
